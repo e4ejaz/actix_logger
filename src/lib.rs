33851375c8227aba67a94fdd8eae03fb5f89da00
e4ejaz <ejazakbar851@gmail.com>
2024-10-18 21:49:25 +0500

    Custom Logstream option added

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,24 @@ macro_rules! log {
         }
     }};
 }
+macro_rules! log_custom {
+    ($level:expr, $log_stream:expr, $($arg:tt)+) => {{
+        let log_to_cloudwatch = std::env::var("LOG_TO_CLOUDWATCH").unwrap_or_else(|_| "false".to_string()) == "true";
+
+        if log_to_cloudwatch {
+            let message_str = format!($($arg)+);
+            let log_stream_clone = $log_stream.clone(); // Clone if necessary
+            tokio::spawn(async move {
+                let _ = $crate::logs::log($level, &message_str, log_stream_clone, file!(), line!()).await;
+            });
+        } else {
+            match $level {
+                log::Level::Error => println!("ERROR: {}", format!($($arg)+)),
+                log::Level::Warn  => println!("WARN: {}", format!($($arg)+)),
+                log::Level::Info  => println!("INFO: {}", format!($($arg)+)),
+                log::Level::Debug => println!("DEBUG: {}", format!($($arg)+)),
+                log::Level::Trace => println!("TRACE: {}", format!($($arg)+)),
+            }
+        }
+    }};
+}
